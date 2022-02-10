43b7f66c3f0963ba71c05db97682ddf0cf503bbf
OldSchool Dev <fam.b.boy2@gmail.com>
2022-02-11 00:16:20 +0700

    update backend

--- backend/models/Cars.js
+++ backend/models/Cars.js
@@ -4,7 +4,28 @@ const CarSchema = new mongoose.Schema({
     brand:String,
     images: String,
     model:String,
-    price: Number
+    showroom: String,
+    price: Number,
+    type: String,
+    engine: String,
+    engineDetails: String,
+    drivetrain: String,
+    transmission: String,
+    fuel: String,
+    fuelSupplySystem: String,
+    upholsteryMat: String,
+    elecSeat: String,
+    sound: String,
+    absBreak: String,
+    airBags: String,
+    elecGlass: String,
+    equipOut: String,
+    equipIn: String,
+    equipSec: String,
+    equipLivable: String,
+    note: String,
+    advice: String,
+    update: String
 })
 
 module.exports = mongoose.model('Car',CarSchema)
