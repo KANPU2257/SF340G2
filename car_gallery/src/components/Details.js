16f9e1f5d6002c55392a03b7963cf87125ee250e
siny2000 <sundress198@gmail.com>
2022-02-11 03:06:05 +0700

    change to TH lang in details

--- car_gallery/src/components/Details.js
+++ car_gallery/src/components/Details.js
@@ -5,26 +5,26 @@ export default class Details extends Component {
     return (
       <div class="detail">
         <img src={require('../assets/testImg.jpeg')} alt="img" id="img" class="subDetail"/>
-        <div id="brand" class="subDetail">brand:</div>
-        <div id="model" class="subDetail">model:</div>
-        <div id="price" class="subDetail">price:</div>
-        <div id="type" class="subDetail">type:</div>
-        <div id="engine" class="subDetail">engine:</div>
-        <div id="detailEngine" class="subDetail">detail of engine:</div>
-        <div id="propulsionSystem" class="subDetail">propulsion system:</div> 
-        <div id="gearSystem" class="subDetail">gear system:</div>
-        <div id="fuelOil" class="subDetail">fuel oil:</div>
-        <div id="fuelOilSystem" class="subDetail">fuel oil system:</div>
-        <div id="upholsteryMaterial" class="subDetail">upholstery material:</div>
-        <div id="electricSeat" class="subDetail">electric seat:</div>
-        <div id="audio" class="subDetail">audio:</div>
-        <div id="breakSystem" class="subDetail">break system:</div>
-        <div id="airBags" class="subDetail">air bags:</div>
-        <div id="electricMirror" class="subDetail">electric mirror:</div>
-        <div id="externalEquipment" class="subDetail">external standard equipment:</div>
-        <div id="internalEquipment" class="subDetail">internal standard equipment:</div>
-        <div id="safetyEquipment" class="subDetail">safety standard equipment:</div>
-        <div id="comfortEquipment" class="subDetail">comfort standard equipment:</div>
+        <div id="brand" class="subDetail">ยี่ห้อ:</div>
+        <div id="model" class="subDetail">ยี่ห้อ-รุ่น:</div>
+        <div id="price" class="subDetail">ราคา:</div>
+        <div id="type" class="subDetail">ประเภท:</div>
+        <div id="engine" class="subDetail">เครื่องยนต์:</div>
+        <div id="detailEngine" class="subDetail">รายละเอียดเครื่องยนต์:</div>
+        <div id="propulsionSystem" class="subDetail">ระบบขับเคลื่อน:</div> 
+        <div id="gearSystem" class="subDetail">ระบบเกียร์:</div>
+        <div id="fuelOil" class="subDetail">เชื้อเพลิงที่ใช้ได้:</div>
+        <div id="fuelOilSystem" class="subDetail">ระบบจ่ายน้ำมัน:</div>
+        <div id="upholsteryMaterial" class="subDetail">วัสดุหุ้มเบาะ:</div>
+        <div id="electricSeat" class="subDetail">เบาะปรับไฟฟ้า:</div>
+        <div id="audio" class="subDetail">เครื่องเสียง:</div>
+        <div id="breakSystem" class="subDetail">ระบบเบรค ABS:</div>
+        <div id="airBags" class="subDetail">Air Bags:</div>
+        <div id="electricMirror" class="subDetail">กระจกไฟฟ้า:</div>
+        <div id="externalEquipment" class="subDetail">อุปกรณ์มาตรฐานภายนอก:</div>
+        <div id="internalEquipment" class="subDetail">อุปกรณ์มาตรฐานภายใน:</div>
+        <div id="safetyEquipment" class="subDetail">อุปกรณ์มาตรฐานความปลอดภัย:</div>
+        <div id="comfortEquipment" class="subDetail">อุปกรณ์มาตรฐานความสะดวกสบาย:</div>
         <div></div>
       </div>
     );
