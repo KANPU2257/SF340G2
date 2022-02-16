e7a0104ca0831a77eef541b7131c6c276250b0a7
OldSchool Dev <fam.b.boy2@gmail.com>
2022-02-16 16:49:12 +0700

    add simpleDetails

--- car_gallery/src/components/DashboardData.js
+++ car_gallery/src/components/DashboardData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CarImgSlider from "./CarImgSlider";
+import SimpleDetail from "./SimpleDetail";
 export default class DashboardData extends Component {
   // เอาไอดีมาเก็บไว้ในสเตดอีกตัวแปรนึงเพื่อค่าจะได้ไม่เปลี่ยน แล้วค่อยใช้ตัวแปรโกลบอลส่งให้ลูก
   render() {
@@ -9,6 +10,7 @@ export default class DashboardData extends Component {
       <div style={{padding:10,marginBottom:30,width:'60%',backgroundColor:'#4600AC',color:'white',borderRadius: 15,boxShadow: '3px 3px black',}}>
         <h1 style={{textShadow:'2px 2px black'}}>{this.props.obj.model}</h1>
         <CarImgSlider slides = {images}></CarImgSlider>
+        <SimpleDetail id = {this.props.obj._id}></SimpleDetail>
       </div>
     );
   }
---
+++ car_gallery/src/components/SimpleDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SimpleDetail extends Component {
+  render() {
+    // console.log(this.props.id);
+    const id = this.props.id;
+    return (
+      <div>{id}</div>
+    )
+  }
+}
