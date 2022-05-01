2e0ca35aeac0cd5c144c76959d1fb7f8848ccfe3
siny2000 <sundress198@gmail.com>
2022-05-01 18:54:17 +0700

    update eachcar (80%)

--- car_gallery/src/components/CarImgSlider.js
+++ car_gallery/src/components/CarImgSlider.js
@@ -3,13 +3,13 @@ import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 const CarImgSlider = (props) => {
   const [current, setCurrent] = useState(0);
-  const [len, setLen] = useState(0)
-  const [images, setImages] = useState([])
-  
-  useEffect(()=>{
-    setImages(props.slides)
-    setLen(images.length) 
-  })
+  const [len, setLen] = useState(0);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    setImages(props.slides);
+    setLen(images.length);
+  });
 
   const nextSlide = () => {
     setCurrent(current === len - 1 ? 0 : current + 1);
@@ -19,32 +19,49 @@ const CarImgSlider = (props) => {
     setCurrent(current === 0 ? len - 1 : current - 1);
   };
 
-//   !Array.isArray(slides) || 
+  //   !Array.isArray(slides) ||
   if (len <= 0) {
     return null;
   }
 
   return (
-    <section className="slider">
-      <FaArrowCircleLeft style={{color:'white'}} className="left-arrow" onClick={prevSlide} />
-      <FaArrowCircleRight style={{color:'white'}} className="right-arrow" onClick={nextSlide} />
-      {images.map((slide,index) => {
+    <section
+      className="slider"
+      // style={{
+      //   width: "100%",
+      //   height: "100%",
+      // }}
+    >
+      <FaArrowCircleLeft
+        style={{ color: "white",left:"8%" }}
+        className="left-arrow"
+        onClick={prevSlide}
+      />
+      <FaArrowCircleRight
+        style={{ color: "white", right:"8%" }}
+        className="right-arrow"
+        onClick={nextSlide}
+      />
+      {images.map((slide, index) => {
         return (
           <div
             className={index === current ? "slide active" : "slide"}
             key={index}
           >
             {index === current && (
-              <img 
-              style={{
-                width:'100%',
-                height:'100%',
-              }} src={slide} alt="car image" className="image"/>
+              <img
+                style={{
+                  width: "600px",
+                  height: "380px",
+                }}
+                src={slide}
+                alt="car image"
+                className="image"
+              />
             )}
           </div>
         );
       })}
-      
     </section>
   );
 };
